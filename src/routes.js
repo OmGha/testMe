3368fd262c50f8fc5041438dc360d11bc6ab7fc5
ToqaMo <toqa9713@hotmail.com>
2019-02-28 11:14:25 +0200

    Toqa Update2 28/2

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,8 @@ import Language from "@material-ui/icons/Language";
 // core components/views for Admin layout
 import DashboardPage from "views/Dashboard/Dashboard.jsx";
 import InstructorDashboardPage from "views/InstructorDashboard/InstructorDashboard.jsx";
-import CoursesList from "views/CoursesList/CoursesList.jsx";
+import StudentCoursesList from "views/StudentCoursesList/StudentCoursesList.jsx";
+import InstructorCoursesList from "views/InstructorCoursesList/InstructorCoursesList.jsx";
 import UserProfile from "views/UserProfile/UserProfile.jsx";
 import Icons from "views/Icons/Icons.jsx";
 import NotificationsPage from "views/Notifications/Notifications.jsx";
@@ -18,49 +19,63 @@ import RTLPage from "views/RTLPage/RTLPage.jsx";
 const dashboardRoutes = [
 
 //STUDENT'S DASHBOARD
-  //{
-  //  path: "/dashboard",
-  //  name: "Dashboard",
-  //  rtlName: "لوحة القيادة",
-  //  icon: Dashboard,
-  //  component: DashboardPage,
-  //  layout: "/admin"
-  //  },
+  {
+   path: "/dashboard",
+   name: "Dashboard",
+   rtlName: "لوحة القيادة",
+   icon: Dashboard,
+   component: DashboardPage,
+   layout: "/admin"
+   },
 
 //INSTRUCTOR'S DASHBOARD
-    {
-        path: "/dashboard",
-        name: "Dashboard",
-        rtlName: "لوحة القيادة",
-        icon: Dashboard,
-        component: InstructorDashboardPage,
-        layout: "/admin"
-    },
+    // {
+    //     path: "/dashboard",
+    //     name: "Dashboard",
+    //     rtlName: "لوحة القيادة",
+    //     icon: Dashboard,
+    //     component: InstructorDashboardPage,
+    //     layout: "/admin"
+    // },
 //optional
-  {
-    path: "/user",
-    name: "User Profile",
-    rtlName: "ملف تعريفي للمستخدم",
-    icon: Person,
-    component: UserProfile,
-    layout: "/admin"
-  },
+  // {
+  //   path: "/user",
+  //   name: "User Profile",
+  //   rtlName: "ملف تعريفي للمستخدم",
+  //   icon: Person,
+  //   component: UserProfile,
+  //   layout: "/admin"
+  // },
+
+  //STUDENT'S COURSESLIST
   {
     path: "/CoursesList",
     name: "Courses List",
     rtlName: "قائمة الجدول",
     icon: "content_paste",
-    component: CoursesList,
+    component: StudentCoursesList,
 
 },
-  {
-    path: "/QuestionBank",
-    name: "Question Bank",
-    rtlName: "بنك الأسئلة",
-    icon: "content_paste",
-    component: QuestionBank,
-    layout: "/admin"
-  }
+
+ //INSTRUCTOR'S COURSESLIST
+//  {
+//   path: "/CoursesList",
+//   name: "Courses List",
+//   rtlName: "قائمة الجدول",
+//   icon: "content_paste",
+//   component: InstructorCoursesList,
+
+// },
+
+//INSTRUCTOR'S QB
+  // {
+  //   path: "/QuestionBank",
+  //   name: "Question Bank",
+  //   rtlName: "بنك الأسئلة",
+  //   icon: "content_paste",
+  //   component: QuestionBank,
+  //   layout: "/admin"
+  // }
   //{
   //  path: "/icons",
   //  name: "Icons",
